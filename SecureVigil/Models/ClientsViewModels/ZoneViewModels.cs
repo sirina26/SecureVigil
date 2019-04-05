5b65726f2a1765f34abe78d9f1e0fea4aa05263f
Marius <yovomarius@gmail.com>
2019-04-05 13:11:11 +0200

    Création du modèle gestion clientèle

---
+++ SecureVigil/Models/ClientsViewModels/ZoneViewModels.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SecureVigil.WebApp.Models.ClientsViewModels
+{
+    public class ZoneViewModels
+    {
+        [Required]
+        [Display (Name = "Zone Name")]
+        public string ZoneName { get; set; }
+
+        [Required]
+        [Display (Name = "Zone Adresse")]
+        public string ZoneAdresse { get; set; }
+
+        [Display (Name ="Longitude")]
+        public double Longitude { get; set; }
+
+        [Display (Name ="Latitude")]
+        public double Latitude { get; set; }
+
+        [Required]
+        [Display (Name = "Contrat")]
+        public int ContratId { get; set; }
+
+    }
+}
