342f194c292a25941da7b5aa653dbb9ae950cd89
Silina <sassi@intechinfo.fr>
2019-04-08 10:46:08 +0200

    Push to recupere

---
+++ SecureVigil/Controllers/ClientController.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using SecureVigil.DAL;
+using SecureVigil.WebApp.Authentication;
+using SecureVigil.WebApp.Models.ClientViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+
+namespace SecureVigil.WebApp.Controllers
+{
+    [Route( "api/[controller]" )]
+    [Authorize( AuthenticationSchemes = JwtBearerAuthentication.AuthenticationScheme )]
+
+    public class ClientController
+    {
+    }
+}
